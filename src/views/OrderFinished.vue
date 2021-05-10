edab2a46b6eed320ddce16cd5c87171852081758
KIYO1234 <getstarted3601@gmail.com>
2021-05-10 13:54:50 +0900

    OrderFinished, RegisterUserコンポーネントを作成し、HTMLをベタ貼りしました

---
+++ src/views/OrderFinished.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+						aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href=""> <!-- 企業ロゴ --> <img
+						alt="main log" src="" height="35">
+					</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse"
+					id="bs-example-navbar-collapse-1">
+					<p class="navbar-text navbar-right">
+						<a href="" class="navbar-link">ショッピングカート</a>&nbsp;&nbsp;
+						<a href="" class="navbar-link">注文履歴</a>&nbsp;&nbsp;
+						<a href="" class="navbar-link">ログイン</a>&nbsp;&nbsp;
+						<a href="" class="navbar-link">ログアウト</a>
+					</p>
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
+
+
+		<!-- table -->
+		<div class="row">
+			<div
+				class="table-responsive col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-10 col-xs-12">
+				<h3 class="text-center">決済が完了しました！</h3>
+				<div class="text-center">
+					<p>この度はご注文ありがとうございます。</p>
+					<p>お支払い先は、お送りしたメールに記載してありますのでご確認ください。</p>
+					<p>メールが届かない場合は「注文履歴」からご確認ください。</p>
+				</div>
+			</div>
+		</div>
+
+		<br><br>
+		<div class="row">
+			<div class="col-xs-offset-5 col-xs-2">
+				<div class="form-group">
+					<form action="order_finished.html">
+						<input class="form-control btn btn-warning btn-block"
+							type="submit" value="トップ画面を表示する">
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default ({
+    name: 'OrderFinished'
+})
+</script>
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="container">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+						aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href=""> <!-- 企業ロゴ --> <img
+						alt="main log" src="" height="35">
+					</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse"
+					id="bs-example-navbar-collapse-1">
+					<p class="navbar-text navbar-right">
+						<a href="" class="navbar-link">ショッピングカート</a>&nbsp;&nbsp;
+					</p>
+				</div>
+				<!-- /.navbar-collapse -->
+			</div>
+			<!-- /.container-fluid -->
+		</nav>
+		
+		<!-- login form -->
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-6 col-md-offset-2 col-md-8 col-sm-10 col-xs-12">
+				<div class="well">
+					<form method="post" action="item_list_pizza.html">
+						<fieldset>
+							<legend>ユーザ登録</legend>
+							<div class="form-group">
+								<label for="inputName">名前:</label><label
+									class="control-label" style="color: red" for="inputError">名前を入力してください</label>
+								<input type="text" id="inputName" class="form-control"
+									placeholder="Name">
+							</div>
+							<div class="form-group">
+								<label for="inputEmail">メールアドレス:</label><label
+									class="control-label" style="color: red" for="inputError">メールアドレスを入力してください</label>
+								<input type="text" id="inputEmail" class="form-control"
+									placeholder="Email">
+							</div>
+							<div class="form-group">
+								<label for="inputZipcode">郵便番号:</label>
+								<label
+									class="control-label" style="color: red" for="inputError">郵便番号を入力してください</label>
+									<input type="button" value="住所検索">
+								<input type="text" id="inputZipcode" class="form-control"
+									placeholder="Zipcode">
+								
+							</div>
+							<div class="form-group">
+								<label for="inputAddress">住所:</label>
+								<label
+									class="control-label" style="color: red" for="inputError">住所を入力してください</label>
+								<input type="text" id="inputAddress" class="form-control"
+									placeholder="Address">
+							</div>
+							<div class="form-group">
+								<label for="inputTel">電話番号:</label>
+								<label
+									class="control-label" style="color: red" for="inputError">電話番号を入力してください</label>
+								<input type="text" id="inputTel" class="form-control"
+									placeholder="Tel">
+							</div>
+							<div class="form-group">
+								<label for="inputPassword">パスワード:</label>
+								<label
+									class="control-label" style="color: red" for="inputError">パスワードを入力してください</label>
+								<input type="text" id="inputPassword" class="form-control"
+									placeholder="Password">
+							</div>
+							<div class="form-group">
+								<label for="inputConfirmationPassword">確認用パスワード:</label>
+								<label
+									class="control-label" style="color: red" for="inputError">確認用パスワードを入力してください</label>
+								<input type="text" id="inputConfirmationPassword" class="form-control"
+									placeholder="Confirmation Password">
+							</div>
+							<div class="form-group">
+								<button type="submit" class="btn btn-primary">登録</button>
+								<button type="reset" class="btn btn-primary">クリア</button>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</template>
+
+<script>
+export default {
+    name: 'RegisterUser'
+}
+</script>

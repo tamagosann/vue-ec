d3225ab8854a10c6288736b63cbc0816018a3338
KIYO1234 <getstarted3601@gmail.com>
2021-05-11 09:59:13 +0900

    OrderConfirmを修正 UserInfoコンポーネントを作成しました

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -1,89 +1,5 @@
 <template>
-    <div class="container">
-        <nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed"
-                        data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
-                        aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span> <span
-                            class="icon-bar"></span> <span class="icon-bar"></span> <span
-                            class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand"> <!-- 企業ロゴ --> <img
-                        alt="main log" src="" height="35">
-                    </a>
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-
-				<!-- リンク -->
-                <div class="collapse navbar-collapse"
-                    id="bs-example-navbar-collapse-1">
-                    <p class="navbar-text navbar-right">
-                        <a href="" class="navbar-link">ショッピングカート</a>&nbsp;&nbsp;
-                        <a href="" class="navbar-link">注文履歴</a>&nbsp;&nbsp;
-                        <a href="" class="navbar-link">ログイン</a>&nbsp;&nbsp;
-                        <a href="" class="navbar-link">ログアウト</a>
-                    </p>
-                </div>
-                <!-- /.navbar-collapse -->
-            </div>
-        </nav>
-
-        <!-- table -->
-		<div class="row">
-			<div
-				class="table-responsive col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-10 col-xs-12">
-				<h3 class="text-center">注文内容確認</h3>
-
-			
-				<table class="table table-striped item-list-table">
-					<tbody>
-						<tr>
-							<th v-for="header in tableHeaders" :key="header.title" width="200px">
-								<span class="text-center">
-									{{header.title}}
-								</span>
-							</th>
-						</tr>
-
-						<tr v-for="item in items" :key="item.id">
-							<td>
-								<div class="center">
-									<img src="../assets/images/1.jpg"
-										class="img-responsive img-rounded item-img-center" width="150" height="150">
-										<br>
-								</div>
-							</td>
-							<td width="100px">
-								<div>
-									{{item.name}}
-								</div>
-								<div class="price">
-									{{item.price}}円	
-								</div>
-							</td>
-						</tr>
-						
-					</tbody>
-				</table>
-
-			</div>
-		</div>
-
-        <div class="row">
-			<div class="col-xs-offset-2 col-xs-8">
-				<div class="form-group text-center">
-					<!-- <div id="total-price">合計金額：{{totalPrice()}}円（税込）</div><br> -->
-					<!-- <button @click="calc()">計算</button> -->
-					<div id="total-price">合計金額：{{totalPrice}}円（税込）</div><br>
-				</div>
-			</div>
-		</div>
-
-        <!-- table（お届け先情報） -->
+    <!-- table（お届け先情報） -->
 		<form action="order_finished.html">
 			<div class="row">
 				<div
@@ -308,12 +224,10 @@
 				</div>
 			</div>
 		</form>
-
-    </div>
-    
 </template>
 
 <script>
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -421,11 +335,3 @@ export default({
 
 })
 </script>
-
-<style>
-	.table{
-		margin-left: auto;
-		margin-right: auto;
-	}
-
-</style>
